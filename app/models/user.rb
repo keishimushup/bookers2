7c2b1a05ef2e83c94d06db97c831a1d662aa3c61
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-07-24 07:42:23 +0000

    [Update]4課題3 index,show,edit作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
-  has_one_attached :prfile_image
+  has_one_attached :profile_image
 
   def get_profile_image(width, height)
     unless profile_image.attached?
