1d61161e45f15e3c5d73ea6c67a8c85d2e1fa17b
keishimushup <108566690+keishimushup@users.noreply.github.com>
2022-07-25 04:29:18 +0000

    [Update]4課題3 destroy実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+     redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def index
@@ -21,8 +26,21 @@ class BooksController < ApplicationController
   end
 
   def edit
+     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(user_params)
+    redirect_to book_path(@book.id)
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
 
   private
 
